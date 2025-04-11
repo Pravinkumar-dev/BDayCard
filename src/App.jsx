18b01177970188198894542f7f75a2df6253fd86
Pravinkumar <Pravinkumar@MacBookAir.lan>
2025-04-11 16:58:44 +0530

    dynamic screen size changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,33 @@
+import { useEffect } from "react";
 import "./App.css";
 import Screen from "./pages/mainScreen/Screen";
 
 function App() {
-  return <Screen />;
+  useEffect(() => {
+    const setViewportHeight = () => {
+      const vh = window.innerHeight * 0.01;
+      document.documentElement.style.setProperty("--vh", `${vh}px`);
+      console.log(vh, 'vh')
+    };
+
+    setViewportHeight();
+    window.addEventListener("resize", setViewportHeight);
+
+    return () => {
+      window.removeEventListener("resize", setViewportHeight);
+    };
+  }, []);
+
+  return (
+    <main
+      style={{
+        height: "calc(var(--vh, 1vh) * 100)",
+        overflow: "hidden",
+      }}
+    >
+      <Screen />
+    </main>
+  );
 }
 
 export default App;
